f6a99e0e1c398e8f8a7f018c49a2211e04ff3dc9
lighthookyu <84956726+lighthookyu@users.noreply.github.com>
2022-04-14 22:56:56 +0800

    Add files via upload

---
+++ gzhu.py
@@ -0,0 +1,388 @@
+import json
+import requests
+import re
+import execjs
+from lxml import html
+import sys
+import os
+
+
+def get_resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def js_from_file(file_name):
+    with open(file_name, 'r', encoding='UTF-8') as file:
+        result = file.read()
+    return result
+
+
+def get_rsa(un, psd, lt):
+    js_path = get_resource_path(r'des.js')
+    context = execjs.compile(js_from_file(js_path))
+    result = context.call("strEnc", un + psd + lt, '1', '2', '3')
+    return result
+
+
+class GZHU(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.client = requests.session()
+
+        self.xkkz_dict = {
+            # '通识': [],
+            # '体育': [],
+            # '主修': [],
+        }
+        # key:['主修课程', '板块课(体育4)', '通识选修', '其他特殊课程']
+        # 每个value大概是：['01', 'D454960F86CF7DFDE053206411AC34BB', '2020', '6590']
+        # 代表：kklxdm、xkkz_id、njdm_id、zyh_id
+
+        self.jg_id = ''
+        self.bh_id = ''
+        self.ccdm = ''
+        self.kklxdm = ''
+        self.njdmId = ''
+        self.xbm = ''
+        self.xkkzId = ''
+        self.xkxnm = ''
+        self.xkxqm = ''
+        self.xqh_id = ''
+        self.xsbj = ''
+        self.xslbdm = ''
+        self.zyfx_id = ''
+        self.zyh_id = ''
+        self.jxbzb = ''
+
+        self.bklx_id = ''
+        self.rwlx = ''
+        self.xkly = ''
+        self.xklc = ''  # 选课轮次
+        self.sfkknj = ''
+        self.sfkkzy = ''
+        self.sfkgbcx = ''
+        self.tykczgxdcs = ''
+        self.rlkz = ''
+
+        self.xkzgbj = ''
+
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+            # 'Accept': 'application/json, text/javascript, */*; q=0.01',
+            # 'Accept-Encoding': 'gzip, deflate',
+            # 'Accept-Language': 'zh-CN,zh;q=0.9',
+            # 'Cache-Control': 'no-cache',
+            # 'Connection': 'keep-alive',
+            # 'Content-Length': '340',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+            'Host': 'jwxt.gzhu.edu.cn',
+            'Origin': 'http://jwxt.gzhu.edu.cn',
+            # 'Pragma': 'no-cache',
+            'Referer': 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default'
+                       '&su={}'.format(self.username),
+        }
+        self.base_headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+        }
+
+    def login(self):
+        new_cas_url = 'https://newcas.gzhu.edu.cn/cas/login'
+        res = self.client.get(new_cas_url)
+        lt = re.findall(r'name="lt" value="(.*)"', res.text)
+
+        login_form = {
+            'rsa': get_rsa(self.username, self.password, lt[0]),
+            'ul': len(self.username),
+            'pl': len(self.password),
+            'lt': lt[0],
+            'execution': 'e1s1',
+            '_eventId': 'submit',
+        }
+
+        resp = self.client.post(new_cas_url, data=login_form,)
+        # print('[debug]:{}'.format(resp.url))
+        selector = html.fromstring(resp.text)
+        if selector.xpath('//title/text()')[0] == '融合门户':
+            jwxt_login = 'http://jwxt.gzhu.edu.cn/sso/driot4login'
+            res = self.client.get(url=jwxt_login, headers=self.base_headers)
+            return True
+        else:
+            return False
+
+    def xuan_ke(self):
+        """
+        加载教务系统,获取
+            jg_id:32位哈希值
+            bh_id:9位数字
+
+        :return:
+            设置self.jg_id: 后续查询需要用到，
+        """
+
+        jwxt_url = 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzb_cxZzxkYzbIndex.html?' \
+                   'gnmkdm=N253512&layout=default&su={}'.format(self.username)
+        resp = self.client.get(url=jwxt_url, headers=self.base_headers)
+        # print('[debug]:{}'.format(resp))
+
+        if '本学期选课要求' in resp.text:
+            selector = html.fromstring(resp.text)
+            self.jg_id = selector.xpath('//input[@name="jg_id_1"]/@value')[0]
+            self.bh_id = selector.xpath('//input[@name="bh_id"]/@value')[0]
+            self.ccdm = selector.xpath('//input[@name="ccdm"]/@value')[0]
+            # self.kklxdm = selector.xpath('//input[@name="firstKklxdm"]/@value')[0]
+            self.njdmId = selector.xpath('//input[@name="firstNjdmId"]/@value')[0]
+            self.xbm = selector.xpath('//input[@name="xbm"]/@value')[0]
+            # self.xkkzId = selector.xpath('//input[@name="firstXkkzId"]/@value')[0]
+            self.xkxnm = selector.xpath('//input[@name="xkxnm"]/@value')[0]
+            self.xkxqm = selector.xpath('//input[@name="xkxqm"]/@value')[0]
+            self.xqh_id = selector.xpath('//input[@name="xqh_id"]/@value')[0]
+            self.xsbj = selector.xpath('//input[@name="xsbj"]/@value')[0]
+            self.xslbdm = selector.xpath('//input[@name="xslbdm"]/@value')[0]
+            self.zyfx_id = selector.xpath('//input[@name="zyfx_id"]/@value')[0]
+            self.zyh_id = selector.xpath('//input[@name="zyh_id"]/@value')[0]
+
+            # 这个参数第二轮出现，第一轮和第三轮暂时不清楚
+            self.jxbzb = selector.xpath('//input[@name="jxbzb"]/@value')[0]
+
+            tab_data = selector.xpath('//a[@data-toggle="tab"]/@onclick')
+            tab_label = selector.xpath('//a[@data-toggle="tab"]/text()')
+            for i in range(len(tab_label)):
+                self.xkkz_dict.update({
+                    tab_label[i]: tab_data[i][18:-2].split("','")
+                    # queryCourse(this,'01','D454960F86CF7DFDE053206411AC34BB','2020','6590')
+                    # 变成
+                    # ['01', 'D454960F86CF7DFDE053206411AC34BB', '2020', '6590']
+                })
+            return True
+        else:
+            return False
+
+    def get_tab_data(self, block: str):
+        """
+        刷新该tab下的数据
+        :param block: 关键字包含就行
+        :return:
+        """
+        for i in self.xkkz_dict.keys():
+            if block in i:
+                # 根据xkkz_id刷新几个数据：bklx、xkly、rwlx。每次请求都要调用的
+                jwxt_url = 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzb_cxZzxkYzbDisplay.html?' \
+                           'gnmkdm=N253512&layout=default&su={}'.format(self.username)
+                data = {
+                    'xkkz_id': self.xkkz_dict[i][1],
+                    'xszxzt': '1',
+                    'kspage': '0',
+                    'jspage': '0',
+                }
+                resp = self.client.post(url=jwxt_url, data=data, headers=self.base_headers,)
+                selector = html.fromstring(resp.text)
+                self.bklx_id = selector.xpath('//input[@name="bklx_id"]/@value')[0]
+                self.rwlx = selector.xpath('//input[@name="rwlx"]/@value')[0]
+                self.xkly = selector.xpath('//input[@name="xkly"]/@value')[0]
+                self.xklc = selector.xpath('//input[@name="xklc"]/@value')[0]
+                self.sfkknj = selector.xpath('//input[@name="sfkknj"]/@value')[0]
+                self.sfkkzy = selector.xpath('//input[@name="sfkkzy"]/@value')[0]
+                self.sfkgbcx = selector.xpath('//input[@name="sfkgbcx"]/@value')[0]
+                self.tykczgxdcs = selector.xpath('//input[@name="tykczgxdcs"]/@value')[0]
+                self.rlkz = selector.xpath('//input[@name="rlkz"]/@value')[0]
+
+                if self.xklc == '2':
+                    self.xkzgbj = selector.xpath('//input[@name="xkzgbj"]/@value')[0]
+
+                # 返回kklxxdm、xkkzid
+                return self.xkkz_dict[i][0], self.xkkz_dict[i][1],
+
+    def search_kch(self, keyword: str, block: str) -> list or None:
+        """
+
+        :param keyword:搜索关键词
+        :param block: 三个板块：主修课程、体育课程、通识选修
+        :return:
+        """
+        search_url = 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzb_cxZzxkYzbPartDisplay.html?gnmkdm=N253512' \
+                     '&su={}'.format(self.username)
+
+        # 都要刷新
+        kklxxdm, xkkzid = self.get_tab_data(block)
+
+        data = {
+            'bh_id': self.bh_id,
+            'bklx_id': self.bklx_id,
+            'ccdm': self.ccdm,
+            'filter_list[0]': str(keyword),     # 这里输入中文
+            'jg_id': self.jg_id,
+            'jspage': '10',
+            'kkbk': '0',
+            'kkbkdj': '0',
+            'kklxdm': kklxxdm,
+            'kspage': '1',
+            'njdm_id': self.njdmId,
+            'rlkz': self.rlkz,
+            'rwlx': self.rwlx,
+            'sfkcfx': '0',
+            'sfkgbcx': self.sfkgbcx,
+            'sfkknj': self.sfkknj,
+            'sfkkzy': self.sfkkzy,
+            'sfkxq': '0',
+            'sfrxtgkcxd': '1',
+            'sfznkx': '0',
+            'tykczgxdcs': self.tykczgxdcs,
+            'xbm': self.xbm,
+            'xkly': self.xkly,
+            'xkxnm': self.xkxnm,
+            'xkxqm': self.xkxqm,
+            'xqh_id': self.xqh_id,
+            'xsbj': self.xsbj,
+            'xslbdm': self.xslbdm,
+            'zdkxms': '0',
+            'zyfx_id': self.zyfx_id,
+            'zyh_id': self.zyh_id,
+        }
+        if self.xklc != '1':
+            data.update(
+                {
+                    "xkzgbj": self.xkzgbj,
+                    "jxbzb": self.jxbzb,
+                }
+            )
+        resp = self.client.post(url=search_url, data=data, headers=self.base_headers,)
+        # print('[debug]:{}'.format(resp.url))
+        search_result = json.loads(resp.text)['tmpList']
+        try:
+            search_data = []
+            for data in search_result:
+                search_data.append({
+                    'course_name': data['kcmc'],    # 课程名称：大学体育4
+                    'kch_id': data['kch_id'],   # 课程号ID：00121704
+                    'jxbmc': data['jxbmc'],     # 教学班名称：(2021-2022-2)-00121704-27
+                    'xf': data['xf'],           # 学分： 1.0
+                    'totalResult': data['yxzrs'],  # 已选人数：0
+                    # 'found': True               # 作为已查找到的标记
+                })
+            return search_data
+            # [{
+            #   'course_name':'xxx',
+            #   'kch_id': '1234',
+            #   ...
+            # },...]
+        except Exception as e:
+            print('SEARCH_ERROR:{}'.format(e))
+            return None
+
+    def query_task(self, keyword: str, kch: str, block: str) -> list or None:
+        """
+        根据课程号查询具体课程
+        :return:
+        """
+        query_task_url = 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzbjk_cxJxbWithKchZzxkYzb.html?gnmkdm=N253512' \
+                         '&su={}'.format(self.username)
+        kklxxdm, xkkzid = self.get_tab_data(block)
+
+        data = {
+            'bh_id': self.bh_id,
+            'bklx_id': self.bklx_id,
+            'ccdm': self.ccdm,
+            'cxbj': '0',      # 重修标记
+            'filter_list[0]': str(keyword),
+            'fxbj': '0',      # 辅修标记
+            'jg_id': self.jg_id,    # 机构id？
+            'kch_id': kch,  # 课程号
+            'kkbk': '0',
+            'kkbkdj': '0',
+            'kklxdm': kklxxdm,
+            'njdm_id': self.njdmId,     # 年级代码
+            'rlkz': self.rlkz,
+            'rwlx': self.rwlx,  # 找不到,应该可以写死
+            'sfkcfx': '0',
+            'sfkknj': self.sfkknj,
+            'sfkkzy': self.sfkkzy,
+            'sfkxq': '0',
+            'sfznkx': '0',
+            'xbm': '1',
+            'xkkz_id': xkkzid,
+            'xkly': self.xkly,      # 选课xx？ 0/2
+            'xkxnm': self.xkxnm,
+            'xkxqm': self.xkxqm,
+            'xqh_id': self.xqh_id,
+            'xsbj': self.xsbj,
+            'xslbdm': self.xslbdm,
+            'zdkxms': '0',    # ?
+            'zyfx_id': self.zyfx_id,
+            'zyh_id': self.zyh_id,  # 专业代号
+        }
+        # output = self.change_data(data)
+        resp = self.client.post(url=query_task_url, data=data, headers=self.headers,)
+        # print(resp)
+        kc_result = json.loads(resp.text)
+        try:
+            query_data = []
+            for data in kc_result:
+                query_data.append({
+                    'do_jxb_id': data['do_jxb_id'],     # 选课教学班id，64位哈希值
+                    'jsxx': re.findall(r'/(.*)/', data['jsxx'])[0],               # 教师信息：103526/纪彦屹/讲师（高校）
+                    # 'totalResult': data['totalResult'], # 推测是已选人数：0
+                    'jxbrl': data['jxbrl'],             # 教学班容量：31
+                    'success': False,                   # 选课成功标记
+                    # 这里添加的信息是课程类型刷新的时候改变的信息,post_do_jxb需要用到
+                    'kklxdm': kklxxdm,
+                    'xkkzid': xkkzid,
+                    'bklx_id': str(self.bklx_id),
+                    'rwlx': str(self.rwlx),
+                    'xkly': str(self.xkly),
+                    'msg': '',  # 存放后面选课返回的信息作为备注
+                })
+            return query_data
+        except Exception as e:
+            print('QUERY ERROR:{}'.format(e))
+            return None
+
+    def post_do_jxb(self, daixuan_data: dict, ):
+        print('正在选课:{}'.format(daixuan_data['course_name']))
+        submit_url = 'http://jwxt.gzhu.edu.cn/jwglxt/xsxk/zzxkyzbjk_xkBcZyZzxkYzb.html?gnmkdm=N253512' \
+                     '&su={}'.format(self.username)
+        kcmc = '({kch}){cn}+-+{xf}+学分'.format(kch=daixuan_data['kch_id'], cn=daixuan_data['course_name'],
+                                              xf=daixuan_data['xf'])
+        data = {
+            'cxbj': '0',
+            'jxb_ids': daixuan_data['do_jxb_id'],
+            'kch_id': daixuan_data['kch_id'],
+            'kcmc': kcmc,
+            'kklxdm': daixuan_data['kklxdm'],
+            'njdm_id': self.njdmId,
+            'qz': '0',
+            'rlkz': self.rlkz,
+            'rlzlkz': '1',
+            'rwlx': daixuan_data['rwlx'],
+            'sxbj': '1',
+            'xkkz_id': daixuan_data['xkkzid'],
+            'xklc': self.xklc,
+            'xkxnm': self.xkxnm,    # 这个不会变
+            'xkxqm': self.xkxqm,
+            'xxkbj': '0',   # todo 注意这个值
+            'zyh_id': self.zyh_id,
+        }
+        resp = self.client.post(url=submit_url, data=data, headers=self.base_headers,)
+
+        submit_result = json.loads(resp.text)
+        if submit_result['flag'] == '1':
+            # 选课成功
+            return True, 'success'
+        elif submit_result['flag'] == '0':
+            # 选课失败
+            return False, submit_result['msg']
+        elif submit_result['flag'] == '-1':
+            return False, '课程已满'
+        else:
+            # 不明原因失败
+            return False, 'SUBMIT ERROR'
+
+    def tuike(self):
+        pass
---
+++ main.py
@@ -0,0 +1,290 @@
+import gzhu
+import os
+import configparser
+import time
+from datetime import datetime
+
+
+class Version(object):
+    def __init__(self):
+        self.version = '1.2'
+        self.release_date = '2022/01/12'
+        self.latest_ver = ''
+        self.latest_rel_date = ''
+
+
+class Config(object):
+    def __init__(self):
+        self.config_ini = configparser.ConfigParser()
+        self.file_path = os.path.join(os.path.abspath('.'), '配置信息.ini')
+        self.config_ini.read(self.file_path, encoding='utf-8')
+
+        base_info = dict(self.config_ini.items('baseinfo'))
+        # self.mode1time = base_info['mode1time']
+        self.username = base_info['username']
+        self.password = base_info['password']
+
+        # 将三个字典的值str->list
+        self.mode1 = self.re_flash('mode1')
+        self.mode2 = self.re_flash('mode2')
+        self.mode3 = self.re_flash('mode3')
+
+    def re_flash(self, section: str) -> dict:
+        """
+
+        :rtype: dict
+        """
+        data = dict(self.config_ini.items(section))
+        for k in data:
+            data[k] = data[k].split(',')
+        return data
+
+    def set_config(self):
+        un = input('输入登录融合门户的学号：')
+        pw = input('输入登录融合门户的密码：')
+        self.config_ini.set('baseinfo', 'username', un)
+        self.config_ini.set('baseinfo', 'password', pw)
+
+        while True:
+            md = input('\n1-自动抢课\n2-捡漏模式\n3-替换模式\n输入你使用的模式前面的数字代号：')
+            if ('1' or '2' or '3') not in md:
+                md = 'mode' + md
+                break
+            print('模式输入错误')
+
+        i = 1   # 计数器
+        while True:
+            print('目前正在设置第{}个教学班设置'.format(i))
+            jxb = input('输入你要选的教学班号:').strip()  # 去掉前后空格
+            bl = input('输入该教学班对应的板块(主修/体育/通识):').strip()
+            self.config_ini.set(md, str(i), '{},{}'.format(jxb, bl))
+
+            end_mark = input('输入0结束录入, 输入其他继续录入')
+            if end_mark == '0':
+                self.config_ini.write(open(self.file_path, 'w', encoding='utf-8'))
+                break
+            i += 1
+
+
+def print_data(data: list):
+    for detail in data:
+        print('课程名称:{course_name}  学分:{xf}分  教师:{jsxx}  容量:{total_result}/{jxbrl}  选课状态:{success}'
+              '  课程号:{kch}  备注:{msg}'.format(course_name=detail['course_name'], xf=detail['xf'],
+                                             jsxx=detail['jsxx'], total_result=detail['totalResult'],
+                                             jxbrl=detail['jxbrl'], success=detail['success'],
+                                             kch=detail['kch_id'], msg=detail['msg']))
+
+
+def get_daixuan_info(xuanke_data: dict) -> list:
+    # 待选信息
+    daixuan_info = []
+    for key in xuanke_data:
+        jxbh = xuanke_data[key][0]  # 教学班号
+        block = xuanke_data[key][1]  # 板块
+
+        search_data = g.search_kch(jxbh, block)
+        if len(search_data) != 0:
+            # 不同教学班共用一个课程号，所以取第一个即可
+            query_data = g.query_task(jxbh, search_data[0]['kch_id'], block)
+
+            """
+            如果正常,search_data应该有这些key
+            'course_name': data['kcmc'],        # 课程名称：大学体育4
+            'kch_id': data['kch_id'],           # 课程号ID：00121704
+            'jxbmc': data['jxbmc'],             # 教学班名称：(2021-2022-2)-00121704-27
+            'xf': data['xf']                    # 学分： 1.0
+            'do_jxb_id': data['do_jxb_id'],     # 选课教学班id，64位哈希值
+            'jsxx': data['jsxx'],               # 教师信息：103526/纪彦屹/讲师（高校）
+            'totalResult': data['totalResult'], # 推测是已选人数：0
+            'jxbrl': data['jxbrl']              # 教学班容量：31
+            'found': True,                      # 作为已查找到的标记
+            'block': '通识',                     # 板块区分
+            """
+            # 这里发生了转换,sd的类型从list转为dict
+            all_data = {}
+            all_data.update(search_data[0])
+            all_data.update(query_data[0])
+            all_data.update({'block': block})
+            daixuan_info.append(all_data)
+        else:
+            search_data = {
+                'course_name': '',
+                'kch_id': jxbh,
+                'jxbmc': '',
+                'xf': '',
+                'do_jxb_id': '',
+                'jsxx': '',
+                'totalResult': '',
+                'jxbrl': '',
+                # 'found': False
+                'success': False,  # 选课成功标记
+                'kklxdm': '',
+                'xkkzid': '',
+                'bklx_id': '',
+                'rwlx': '',
+                'xkly': '',
+                'msg': '',  # 存放后面选课返回的信息作为备注
+            }
+            daixuan_info.append(search_data)
+    # [{'cousrse_name':"...",...},{...},...]
+    return daixuan_info
+
+
+def xuanke1(xuanke_data: dict):
+    daixuan_info = get_daixuan_info(xuanke_data=xuanke_data)
+    try_time = 1
+    while True:
+        print(''.center(30, '='))
+        print('{time}  正在进行第{tt}次轮询'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), tt=try_time))
+        for daixuan in daixuan_info:
+            if daixuan['course_name'] == '':
+                print('教学班号:{}查找无结果'.format(daixuan['kch_id']))
+                continue
+            if daixuan['success'] is not True:
+                if (daixuan['totalResult'] < daixuan['jxbrl']) or ('通识' in daixuan['block']):
+                    status, msg = g.post_do_jxb(daixuan)
+                    daixuan['success'] = status
+                    daixuan['msg'] = msg
+                    print('{time}  {cn}选课成功'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                    cn=daixuan['course_name']))
+                else:
+                    daixuan['success'] = True
+                    daixuan['msg'] = '已满且为非通选课，已跳过'
+                    print('{cn}已满且为非通选课，已跳过'.format(cn=daixuan['course_name']))
+                    continue
+        # 判断是否全部选完
+        success_mark = []
+        for daixuan in daixuan_info:
+            success_mark.append(daixuan['success'])
+        if False not in success_mark:
+            print('全部选课完成'.center(30, '='))
+            print_data(daixuan_info)
+            break
+        time.sleep(10)
+        try_time += 1
+
+
+def xuanke2(xuanke_data: dict):
+    try_time = 1
+    while True:
+        daixuan_info = get_daixuan_info(xuanke_data=xuanke_data)
+        print(''.center(15, '-'))
+        print('{time}  正在进行第{tt}次轮询'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), tt=try_time))
+        for daixuan in daixuan_info:
+            if daixuan['course_name'] == '':
+                print('教学班号:{}查找无结果'.format(daixuan['kch_id']))
+                continue
+            if daixuan['success'] is not True:
+                status, msg = g.post_do_jxb(daixuan)
+                daixuan['success'] = status
+                daixuan['msg'] = msg
+                if status:
+                    print('{time}  {cn}选课成功'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                    cn=daixuan['course_name']))
+                else:
+                    if msg == '一门课程只能选一个教学班，不可再选！':  # 没有必要再选下去，因此直接化成True即可
+                        daixuan['success'] = True
+                    print('{time}  {cn}选课失败  原因：{msg}'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                              cn=daixuan['course_name'], msg=daixuan['msg']))
+        # 判断是否全部选完
+        success_mark = []
+        for daixuan in daixuan_info:
+            success_mark.append(daixuan['success'])
+        if try_time % 10 == 0:
+            print('\n\n')
+            print(''.center(30, '='))
+            print_data(daixuan_info)
+        if False not in success_mark:
+            print('\n\n')
+            print('全部选课完成'.center(30, '='))
+            print_data(daixuan_info)
+            break
+        time.sleep(0.1)
+        try_time += 1
+
+
+def xuanke3(xuanke_data: dict):
+    try_time = 1
+    while True:
+        daixuan_info = []
+        for d in xuanke_data:
+            tmp = {
+                '1': [xuanke_data[d][0], xuanke_data[d][2]],
+                '2': [xuanke_data[d][1], xuanke_data[d][2]],
+            }
+            row_data = get_daixuan_info(xuanke_data=tmp)
+            daixuan_info.append(row_data)
+        print(daixuan_info)
+        print(''.center(15, '-'))
+        print('{time}  正在进行第{tt}次轮询'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), tt=try_time))
+
+
+        try_time += 1
+
+
+if __name__ == '__main__':
+    bannerstr = r"""
+
+ _     ___ ____ _   _ _____ _   _  ___   ___  _  __
+| |   |_ _/ ___| | | |_   _| | | |/ _ \ / _ \| |/ /
+| |    | | |  _| |_| | | | | |_| | | | | | | | ' / 
+| |___ | | |_| |  _  | | | |  _  | |_| | |_| | . \ 
+|_____|___\____|_| |_| |_| |_| |_|\___/ \___/|_|\_\
+"""
+    menustr = """
+    Course-Bullying-in-GZHU
+    0:配置信息助手
+    1:登录教务系统
+    2:关于信息(!)
+    9:退出                                                   
+"""
+    verinfo = Version()
+    while True:
+        os.system('cls')
+        print(bannerstr)
+        print(menustr)
+        choice_menu = input('\n输入操作前面的数字标号，按回车确定:')
+
+        if choice_menu == '0':
+            config = Config()
+            config.set_config()
+        elif choice_menu == '1':
+            config = Config()
+            print('正在登录教务系统中...')
+            g = gzhu.GZHU(config.username, config.password)
+            if g.login() and g.xuan_ke():
+                print('\n登录成功!当前用户为{un}'.format(un=config.username))
+                while True:
+                    print('1：自动抢课（不推荐）\n2：捡漏模式（推荐）\n3：替换模式（没做）\n99：返回主界面')
+                    choice_xuanke = input('请输入选课模式,按回车确定:')
+                    if choice_xuanke == '1':
+                        xuanke1(config.mode1)
+                    elif choice_xuanke == '2':
+                        xuanke2(config.mode2)
+                    elif choice_xuanke == '3':
+                        xuanke3(config.mode3)
+                    elif choice_xuanke == '99':
+                        break
+                    else:
+                        print('输入有误!')
+                        os.system('pause')
+                        continue
+                    os.system('pause')
+            else:
+                print('登录失败!请检查用户名和密码是否正确!高峰时期可多试几次！')
+                os.system('pause')
+        elif choice_menu == '2':
+            os.system('cls')
+            infostr = """
+    GZHU抢课脚本,{rel_data}
+    对于任何使用问题或讨论，请联系wuhuqifei@lighthook.xyz
+    注意:使用请严格遵守法律法规和学校规章制度
+    
+    简单可以，简陋不行。
+    """.format(rel_data=verinfo.release_date)
+            print(bannerstr)
+            print(infostr)
+            os.system('pause')
+        else:
+            os._exit(0)
+
